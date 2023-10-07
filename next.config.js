bd220c4050b2aed42a53394d33cce39a7c696d5f
yonghk423 <yonghk423423@gmail.com>
2023-10-07 18:15:26 +0900

    header ui

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "1000logos.net",
+        pathname: "/wp-content/uploads/2021/05/Trello-logo.png",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
